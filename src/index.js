613610ab3f70f6b28c7037ca64c741c7e7acb2ad
ANAND PARMAR <42490588+ianandparmar@users.noreply.github.com>
2021-03-13 08:54:20 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// var React =require("react");
+// var ReactDom =require("react-dom");
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(<h1>Hi I am creating ReactApp</h1>,document.getElementById("root"));
